2d72ee1f3f921805cd59dae92a91eba83ce8ba1e
Avoxel284 <avoxel284@gmail.com>
2023-03-08 10:13:00 +1100

    done

--- 2-semiprimes.js
+++ 2-semiprimes.js
@@ -18,6 +18,7 @@ function checkIfPrime(n) {
 	return n > 1;
 }
 /**
+ * Finds the semiprimes between a given minimum and maximum.
  *
  * @param {Number} min
  * @param {Number} max
@@ -26,13 +27,13 @@ function findSemiprimes(min, max) {
 	if (max < min) throw "Minimum is larger than maximum";
 	const results = [];
 	for (let n = min; n <= max; n++) {
-		// Save time if n is a semi prime through its roots
-		// if (checkIfPrime(Math.sqrt(n))) results.push(n);
-
 		for (let i = 2; i < n; i++) {
+			// If n's factors aren't whole, continue
 			if ((n / i) % 1 != 0) continue;
+			// Get n's factors; a & b
 			let a = n / i,
 				b = i;
+			// If a & b are prime numbers themselves, append them to our results
 			if (checkIfPrime(a) && checkIfPrime(b) && !results.includes(n)) results.push(n);
 
 			// console.log(`${n} = ${a} x ${b}`);
@@ -41,4 +42,25 @@ function findSemiprimes(min, max) {
 	return results;
 }
 
-console.log(findSemiprimes(3, 11));
+inquirer
+	.prompt([
+		{
+			name: "min",
+			message: "Please enter a minimum:",
+			type: "input",
+			validate: (input) => (!Number.isNaN(input) && input > 0 ? true : "Number must be above 0"),
+		},
+		{
+			name: "max",
+			message: "...And please enter a maximum:",
+			type: "input",
+			validate: (input) => (!Number.isNaN(input) && input > 0 ? true : "Number must be above 0"),
+		},
+	])
+	.then((v) => {
+		console.log(
+			`Here are the semiprimes in range ${v.min} to ${v.max}: ${findSemiprimes(v.min, v.max)
+				.map((v, i, a) => `${v}${i < a.length - 1 ? ", " : ""}`)
+				.join("")}`
+		);
+	});
